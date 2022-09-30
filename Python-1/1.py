e8c5bcca199ed1de49d4b2d5a9df70ac2ee1e73a
TusharPatwal <70883944+TusharPatwal@users.noreply.github.com>
2022-09-30 07:39:03 +0530

    Date - 30/09/2022

---
+++ Python-1/1.py
@@ -0,0 +1,32 @@
+#first_name = "Tony"
+#last_name = "Stark"
+#age = 51
+#is_genius = True
+
+#print(first_name, last_name, age, is_genius)
+
+
+#name = input("What is your NAME?")
+
+#print("hello "+ name)
+
+
+#Superhero = input("What is your superhero name?")
+#print(Superhero)
+
+"""
+old_age = input("What is you age?")
+new_age = int(old_age) + 5
+print(new_age)
+"""
+
+
+"""number = 17
+print(float(number))"""
+
+
+first = input("enter first number : ")
+second = input("enter second numdber : ")
+
+sum = int(first) + int(second)
+print("the sum is : " + str(sum))
