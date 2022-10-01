6ea888373740d9fed8bb2b77f67eab2a3536f4d8
TusharPatwal <70883944+TusharPatwal@users.noreply.github.com>
2022-10-01 23:34:31 +0530

    Date -01/10/2022

---
+++ Python-1/2.py
@@ -0,0 +1,43 @@
+from asyncore import read
+from unittest import result
+
+
+name = "Tushar Patwal"
+
+#print(name.upper())
+#print(name.lower())
+#print(name.find('s'))
+#print(name.replace("Tushar Patwal", "King"))
+#print(name)
+
+
+'''print('T' in name)
+print('f' in name)
+'''
+
+
+'''print(5+2)#addition operator
+print(5-2)#minus operator
+print(5*2)#multiply operator
+print(5/2)#divide operator 
+print(5//2)#divide operator(but it not any decimal digit in answer)give 
+print(5%2)#remainder operator
+print(5**3)#power operator
+'''
+
+
+'''i = 5 
+#i = i + 2 #can be written as
+i += 2
+i -= 2 
+i *= 2
+print(i)
+'''
+
+#precedence in operator
+'''result1 = (2 + 3) * 5 
+print(result1)
+
+result2 = 2 + 3 * 5
+print(result2)'''
+
---
+++ Python-1/3.py
@@ -0,0 +1,15 @@
+'''Comparison operator'''
+
+'''print(3>2)# greater operator
+print(3>=2)# greater than = operator
+print(3<2)# Smaller operator
+print(3<=2)# Smaller than = operator
+print(3==3)# = operator
+print(3!=2)# Not = operator'''
+
+
+'''Logical operator'''
+
+print(2>3 or 2>1)# OR operator(When any single condition is true then it gives true)
+print(3>2 and 2>4)# AND operator(When all condition are true then only it gives true)
+print(not 4>1)# NOT operator(It changes the outcome)
