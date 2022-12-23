1a994c9de75fffb773c3708bc370d2ef77fc3a91
TusharPatwal <patwaltushar001@gmail.com>
2022-12-23 14:36:35 +0530

    update

--- Python-1/problem01.py
+++ Python-1/problem01.py
@@ -1,11 +1,17 @@
 import os
-n = int(input("Enter the n: "))
+# n = int(input("Enter the n: "))
 d1 = {}
-for i in range(n):
+# for i in range(n):
+bid_done = False
+while not bid_done:
     name = input("Enter the name: ")
     bid = int(input("Enter the amount: "))
     d1[name] = bid
-    os.system('cls')
+    bid_continue = input("Enter 'yes' if you want to continue the bid or 'no' if not: ")
+    if bid_continue == 'no':
+        bid_done = True
+    elif bid_continue == 'yes':
+        os.system('cls')
     
 a = max(d1.values())
 keys = [k for k, v in d1.items() if v == a]
