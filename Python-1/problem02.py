4a475c6b8bc89d96630e338f53819fef8dc8494b
TusharPatwal <patwaltushar001@gmail.com>
2022-12-24 14:58:41 +0530

    update

---
+++ Python-1/problem02.py
@@ -0,0 +1,7 @@
+# docstring(""" """): it is used show the use function while using
+def sum(n,m):
+    """use for sum of two integer"""
+    return n+m
+
+if __name__ == "__main__":
+    print(sum(2,7))
