e03dee397e4a767181334e03e94bb84c0bdb765f
TusharPatwal <patwaltushar001@gmail.com>
2023-01-07 22:30:32 +0530

    simple arithmetic operations

--- Python-1/p01.py
+++ Python-1/p01.py
@@ -1,20 +1,16 @@
 # This program performs simple arithmetic operations
 
-# Get the user's input
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
 
-# Print a menu of options
 print("\nMenu")
 print("1 - Addition")
 print("2 - Subtraction")
 print("3 - Multiplication")
 print("4 - Division")
 
-# Get the user's choice
 choice = int(input("\nEnter your choice: "))
 
-# Perform the chosen operation
 if choice == 1:
     result = num1 + num2
     print("\nThe result is", result)
