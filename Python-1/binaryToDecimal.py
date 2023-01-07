89a3f0f713bef6598c931af41246c723bf4bfe62
TusharPatwal <patwaltushar001@gmail.com>
2023-01-07 15:57:21 +0530

    Code for binary to decimal

---
+++ Python-1/binaryToDecimal.py
@@ -0,0 +1,10 @@
+def bin_to_decimal(bin_num):
+    decimal_num = 0
+    for digit in bin_num:
+        decimal_num = decimal_num * 2 + int(digit)
+    return decimal_num
+
+print(bin_to_decimal('1010'))  # prints 10
+print(bin_to_decimal('110101'))  # prints 53
+print(bin_to_decimal('1'))  # prints 1
+print(bin_to_decimal('0'))  # prints 0
