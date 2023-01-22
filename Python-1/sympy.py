81feb7ef5de3e4940041824469a675e2e44dbf5d
TusharPatwal <patwaltushar001@gmail.com>
2023-01-22 22:15:22 +0530

    random password generator with string and numbers

--- Python-1/sympy.py
+++ Python-1/sympy.py
@@ -1,3 +1,17 @@
-# cook your dish here
-from sympy import primerange
-print(list(sympy.primerange(0, 100)))
+# # cook your dish here
+# from sympy import primerange
+# print(list(sympy.primerange(0, 100)))
+
+import random
+import string
+
+def generate_password(length):
+    # Use the string library to get a list of all letters and digits
+    characters = string.ascii_letters + string.digits
+    # Use random.sample to choose `length` random elements from `characters`
+    password = ''.join(random.sample(characters, length))
+    return password
+
+# Example usage:
+password = generate_password(36)
+print(password)
