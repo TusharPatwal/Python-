69b42fd2d7f54b4c8150f4953c8fad69e4fcca58
TusharPatwal <patwaltushar001@gmail.com>
2023-03-25 20:31:16 +0530

    exeFile calculator in python

--- Project/exeFile/main.py
+++ Project/exeFile/main.py
@@ -1,11 +1,13 @@
 import os
 
-
-operator = int(input("Enter the operator number(1 +, 2 -, 3 *, 4 /): "))
-
-
+def check():
+    cal = input("Enter 'y' for continuing the program and 'n' for closing the program: ")
+    os.system("cls")
+    while(cal!='n'):
+        return calculator()
 
 def calculator():
+    operator = int(input("Enter the operator number(1 +, 2 -, 3 *, 4 /): "))
     number1 = eval(input("Enter the first number: "))
     number2 = eval(input("Enter the second number: "))
     while(True):
@@ -21,18 +23,9 @@ def calculator():
         elif(operator == 4):
             print(number1/number2)
             break
-        
+        else:
+            print("Error")
+    
+    return check()
+    
 calculator()
-
-cal = input("Enter 'y' for continuing the program and 'n' for closing the program: ")
-
-
-def check(cal):
-    os.system("cls")
-    if(cal=='y'):
-        return calculator()
-    else:
-        return "The end!"
-
-
-check(cal)
